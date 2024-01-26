49af5e70fb91b5a412300dc21ba34100e4bf4ea2
Anna0118 <annachu0988@gmail.com>
2024-01-26 16:23:13 +0800

    add gpt

--- app.py
+++ app.py
@@ -13,10 +13,7 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.getenv('YOUR_CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('YOUR_CHANNEL_SECRET'))
 
-# client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
-
-def generate_response(prompt, role="user"):
-    pass
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -32,11 +29,26 @@ def callback():
 
     return 'OK'
 
+def generate_response(prompt, role="user"):
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            # 賦予他角色
+            {"role": "system", "content": "你是一個很厲害的高級軟體工程師，精通正體中文和英文，根本是一個超專業的助理"},
+            # user給prompt
+            {"role": "user", "content": prompt}
+        ]
+    )
+    return completion.choices[0].message.content
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    user_message = event.message.text
+    bot_response = generate_response(user_message)
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text)
+        TextSendMessage(text=bot_response)
     )
 
 if __name__ == "__main__":
